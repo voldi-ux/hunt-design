b8d2fbe2e4f6ee5ea31d7ec4950c84d27542d99a
voldi <vee156@gmail.com>
2021-04-05 21:25:43 +0200

    added a new a project to the website i.e findme

--- src/data/data.js
+++ src/data/data.js
@@ -3,92 +3,91 @@ import findme from "../assets/images/findme.png";
 import trilo from "../assets/images/image-3.png";
 import natours from "../assets/images/image-4.png";
 
-export const ContentDetails = [{
-    title:'Web Disign',
-    content:`Web design refers to how the website is displayed on the internet and on a user's device. Ensuring that your website is displayed beautifully is my number one priority. The website will be scalable and the codes will be extensible in order to ensure that there is sufficient room for your business to grow and expand`
-},
-{
-    title:'UI/UX',
-    content:`Having an amazing looking website with great user interface and an outstanding user experience is really crucial in today's world. You will get your website or web application that looks brilliant and functions on all devices. The perfect user interface and an astounding user experience will make your website standout  in the market. `
-}, {
-    title:'SEO',
-    content:`Your website will be intergrated with search engine optimization(SEO). SEO,which is the process of improving your website to increase it's visibility and make it rank high in search results. The more visible your website is, the more likely you will amass attention and attract customers to your business.`
-}]
-
+export const ContentDetails = [
+  {
+    title: "Web Disign",
+    content: `Web design refers to how the website is displayed on the internet and on a user's device. Ensuring that your website is displayed beautifully is my number one priority. The website will be scalable and the codes will be extensible in order to ensure that there is sufficient room for your business to grow and expand`,
+  },
+  {
+    title: "UI/UX",
+    content: `Having an amazing looking website with great user interface and an outstanding user experience is really crucial in today's world. You will get your website or web application that looks brilliant and functions on all devices. The perfect user interface and an astounding user experience will make your website standout  in the market. `,
+  },
+  {
+    title: "SEO",
+    content: `Your website will be intergrated with search engine optimization(SEO). SEO,which is the process of improving your website to increase it's visibility and make it rank high in search results. The more visible your website is, the more likely you will amass attention and attract customers to your business.`,
+  },
+];
 
 export const aboutContent = {
-    content:`HuntDesign founded by Voldi Muyumba, a full stack web developer, a freelancer and a software developer, helps businesses of all sorts and sizes with creation of outstanding looking websites and web applications. It also improves user interface, user experience and intergrate search engine optimization, responsive web design for businesses that already have a pre-existing website or web application. HuntDesign has got a mission to not only assists local businesses expand significantly but to go global and aids all businesses around the world.`
-}
-
-
+  content: `HuntDesign founded by Voldi Muyumba, a full stack web developer, a freelancer and a software developer, helps businesses of all sorts and sizes with creation of outstanding looking websites and web applications. It also improves user interface, user experience and intergrate search engine optimization, responsive web design for businesses that already have a pre-existing website or web application. HuntDesign has got a mission to not only assists local businesses expand significantly but to go global and aids all businesses around the world.`,
+};
 
 export const websites = [
-    {
-      title: "SI recording studio website",
-      image: studio,
-      color: "#15B756",
-      description: `A stunning looking website created for a recording studio located in Johannesburg South Africa. The site is responsive with a remarkable user interface and user experience`,
-      link:' https://voldi-ux.github.io/si-studio'
-    },
-    {
-      title: "Findme",
-      image: findme,
-      color: "#0a55b8",
-      description:`Findme is an instant messaging web application that allows you to find all important people and their profiles and allow you to message instantly.Functions on all type of devices.`,
-      link:'https://findme-vol.herokuapp.com/'
-    },
-    {
-      title: "Trilo Website",
-      image: trilo,
-      color: "#ED1E79",
-      description:`Trilo site is an html 5 website template. It has an awesome look and feel and provides an awesome user interface and user experience.`,
-      link:' https://voldi-ux.github.io/trillo-web/'
-    },
-    {
-      title: "Natours Website",
-      image: natours,
-      color: "#15B756",
-      description:`The Natours website is an extraordinary site where you can book for awesome tours with your families or friends and take a tour out in nature.`,
-      link:' https://voldi-ux.github.io/natours'
-    },
-  ];
+  {
+    title: "SI recording studio website",
+    image: studio,
+    color: "#15B756",
+    description: `A stunning looking website created for a recording studio located in Johannesburg South Africa. The site is responsive with a remarkable user interface and user experience`,
+    link: " https://voldi-ux.github.io/si-studio",
+  },
+  {
+    title: "Findme",
+    image: findme,
+    color: "#0a55b8",
+    description: `Findme is an instant messaging web application that allows you to find all important people and their profiles and allow you to message instantly.Functions on all type of devices.`,
+    link: "https://findme-vol.herokuapp.com/",
+  },
+  {
+    title: "Trilo Website",
+    image: trilo,
+    color: "#ED1E79",
+    description: `Trilo site is an html 5 website template. It has an awesome look and feel and provides an awesome user interface and user experience.`,
+    link: " https://voldi-ux.github.io/trillo-web/",
+  },
+  {
+    title: "Natours Website",
+    image: natours,
+    color: "#15B756",
+    description: `The Natours website is an extraordinary site where you can book for awesome tours with your families or friends and take a tour out in nature.`,
+    link: " https://voldi-ux.github.io/natours",
+  },
+];
 
-  export  const packages = [
-    {
-      title: "Static Website",
-      services: [
-        "3 pages",
-        "2 x Designs",
-        "ssl security",
-        "SEO",
-        "Responsive",
-        "1 month maintenance",
-      ],
-    },
-    {
-      title: "Online Shops",
-      services: [
-        "Shopping Cart",
-        "Online Payments",
-        "2 x Designs",
-        "ssl security",
-        "SEO",
-        "Responsive",
-        "2 months maintenance",
-        "Database",
-      ],
-    },
-    {
-      title: "Web Applications",
-      services: [
-        "User Authentication",
-        "Login implementation",
-        "ssl security",
-        "SEO",
-        "Responsive",
-        "3 months maintenance",
-        "Database",
-      ],
-    },
-  ];
-  
+export const packages = [
+  {
+    title: "Static Website",
+    services: [
+      "3 pages",
+      "2 x Designs",
+      "ssl security",
+      "SEO",
+      "Responsive",
+      "1 month maintenance",
+    ],
+  },
+  {
+    title: "Online Shops",
+    services: [
+      "Shopping Cart",
+      "Online Payments",
+      "2 x Designs",
+      "ssl security",
+      "SEO",
+      "Responsive",
+      "2 months maintenance",
+      "Database",
+    ],
+  },
+  {
+    title: "Web Applications",
+    services: [
+      "User Authentication",
+      "Login implementation",
+      "ssl security",
+      "SEO",
+      "Responsive",
+      "3 months maintenance",
+      "Database",
+    ],
+  },
+];
